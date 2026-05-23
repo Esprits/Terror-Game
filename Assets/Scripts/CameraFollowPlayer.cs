c49a1d19e4a5c070e71c0cbe40917992df39eb36
Esprits <lmignacca@outlook.com>
2026-05-23 15:26:43 -0400

    Added Jump
    - Added jump function to the player
    - Added comments and tooltips for clarity

--- Assets/Scripts/CameraFollowPlayer.cs
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -1,19 +1,22 @@
 using UnityEngine;
 
 public class CameraFollowPlayer : MonoBehaviour {
+    [Tooltip("Vertical offset to the player")]
     public float offsetY = 1f;
+    [Tooltip("Horizontal offset to the player\nHas to be negative to be behind the player")]
     public float offsetZ = -3.5f;
-    
-    private GameObject player;
-    private Vector3 playerPosition;
 
-    private void Start() {
+    GameObject player;
+    Vector3 playerPosition;
+
+    void Start() {
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    private void Update() {
+    void Update() {
         playerPosition = player.transform.position;
 
+        // Moves the camera according to the player's position (and adds the offset set in the public variables)
         transform.position = new Vector3(playerPosition.x, playerPosition.y + offsetY, playerPosition.z + offsetZ);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,30 +2,35 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour {
+    [Tooltip("How fast the player moves")]
+    public float moveSpeed = 5f;
+    [Tooltip("How high the player jumps")]
+    public float jumpHeight = 50f;
+
     Rigidbody rb;
+    Vector3 moveDirection;
 
-    public float movementSpeed = 5f;
-
-    Vector3 direction;
-
-    private void Start() {
+    void Start() {
         rb = GetComponent<Rigidbody>();
     }
 
-    private void FixedUpdate() {
-        rb.MovePosition(rb.transform.position + direction * movementSpeed * Time.fixedDeltaTime);
+    void FixedUpdate() {
+        // Constantly checks for change in moveDirection to apply to the player
+        rb.MovePosition(rb.transform.position + moveDirection * moveSpeed * Time.fixedDeltaTime);
     }
 
-    private void OnMove(InputValue input) {
-        direction = new Vector3(input.Get<Vector2>().x, 0, input.Get<Vector2>().y);
+    void OnMove(InputValue input) {
+        // Applies the direction from the input's Vector2 (x, z) to moveDirection
+        moveDirection = new Vector3(input.Get<Vector2>().x, 0, input.Get<Vector2>().y);
     }
 
-    private void OnJump(InputValue input) {
-        if (input.isPressed)
-            Debug.Log("Jump");
+    void OnJump(InputValue input) {
+        if (input.isPressed) // When the button is pressed
+            // Adds a vertical (Vector3.up = new Vector(0, 1, 0)) force to the player, making them jump
+            rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
     }
 
-    private void OnSprint(InputValue input) {
+    void OnSprint(InputValue input) {
         if (input.isPressed)
             Debug.Log("Sprint");
     }
