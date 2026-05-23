42d13d4dd1f6d80ccbfbac4b6d2c6df8bb7f5711
Esprits <lmignacca@outlook.com>
2026-05-23 15:55:01 -0400

    Added Running
    - Added running functionality (while holding shift)
    - Changed default values for moveSpeed, runMult & jumpHeight
    - Replaced "player" by "Terror" in comments

--- Assets/Scripts/CameraFollowPlayer.cs
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 
 public class CameraFollowPlayer : MonoBehaviour {
-    [Tooltip("Vertical offset to the player")]
+    [Tooltip("Vertical offset to Terror")]
     public float offsetY = 1f;
-    [Tooltip("Horizontal offset to the player\nHas to be negative to be behind the player")]
+    [Tooltip("Horizontal offset to Terror\nHas to be negative to be behind him")]
     public float offsetZ = -3.5f;
 
     GameObject player;
@@ -16,7 +16,7 @@ public class CameraFollowPlayer : MonoBehaviour {
     void Update() {
         playerPosition = player.transform.position;
 
-        // Moves the camera according to the player's position (and adds the offset set in the public variables)
+        // Moves the camera according to Terror's position (and adds the offset set in the public variables)
         transform.position = new Vector3(playerPosition.x, playerPosition.y + offsetY, playerPosition.z + offsetZ);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,20 +2,26 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour {
-    [Tooltip("How fast the player moves")]
-    public float moveSpeed = 5f;
-    [Tooltip("How high the player jumps")]
-    public float jumpHeight = 50f;
+    [Tooltip("How fast Terror moves")]
+    public float moveSpeed = 4f;
+    [Tooltip("Multiplier to add to Terror's speed while running")]
+    public float runMult = 1.75f;
+    [Tooltip("How high Terror jumps")]
+    public float jumpHeight = 35f;
 
     Rigidbody rb;
     Vector3 moveDirection;
+    float originalMoveSpeed;
 
     void Start() {
         rb = GetComponent<Rigidbody>();
+
+        // Stores the original speed (when not running)
+        originalMoveSpeed = moveSpeed;
     }
 
     void FixedUpdate() {
-        // Constantly checks for change in moveDirection to apply to the player
+        // Constantly checks for change in moveDirection to apply to Terror
         rb.MovePosition(rb.transform.position + moveDirection * moveSpeed * Time.fixedDeltaTime);
     }
 
@@ -24,14 +30,18 @@ public class PlayerController : MonoBehaviour {
         moveDirection = new Vector3(input.Get<Vector2>().x, 0, input.Get<Vector2>().y);
     }
 
-    void OnJump(InputValue input) {
-        if (input.isPressed) // When the button is pressed
-            // Adds a vertical (Vector3.up = new Vector(0, 1, 0)) force to the player, making them jump
-            rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+    void OnRun(InputValue input) {
+        if (input.isPressed)
+            // When the button is held, multiply the moveSpeed by it's multiplier
+            moveSpeed *= runMult;
+        else
+            // When the button is released, set the moveSpeed to its original value
+            moveSpeed = originalMoveSpeed;
     }
 
-    void OnSprint(InputValue input) {
-        if (input.isPressed)
-            Debug.Log("Sprint");
+    void OnJump(InputValue input) {
+        if (input.isPressed) // When the button is pressed
+            // Adds a vertical (Vector3.up = new Vector(0, 1, 0)) force to Terror, making him jump
+            rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
     }
 }
